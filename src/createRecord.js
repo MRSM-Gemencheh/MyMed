6083fe1bfeecf3892eb1d0561384a0676e5dd652
MagusWyvern <72914828+MagusWyvern@users.noreply.github.com>
2023-07-07 22:50:58 +0800

    Major revamp to adding new records

--- src/createRecord.js
+++ src/createRecord.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { GoogleAuthProvider, signInWithRedirect, getRedirectResult, getAuth, signOut } from "firebase/auth";
-import { getFirestore, collection, query, orderBy, getDocs, addDoc, where } from "firebase/firestore";
+import { getFirestore, collection, query, orderBy, getDocs, addDoc, where, get, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAzlMJB0WkUFXUXaD-GszRKxJayUo8tZQo",
@@ -13,37 +13,34 @@ const firebaseConfig = {
     measurementId: "G-PG5LK8VTP9"
 };
 
-
 initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
 const auth = getAuth();
 
 auth.onAuthStateChanged(function (user) {
-  if (user) {
-      // User is signed in, you can access the user object
-      console.log(user);
+    if (user) {
+        // User is signed in, you can access the user object
+        console.log(user);
 
-      userName.textContent = user.displayName;
-      signOutButton.style.display = "block"
-  } else {
-      // User is signed out
-      console.log("User is not logged in");
+        userName.textContent = user.displayName;
+        signOutButton.style.display = "block"
+    } else {
+        // User is signed out
+        console.log("User is not logged in");
 
-      // Redirect user back to sign in page
+        // Redirect user back to sign in page
 
-      let signInRequiredProgressBar = document.getElementById('signInRequiredProgressBar')
-      let signInRequiredText = document.getElementById('signInRequiredText')
+        let signInRequiredProgressBar = document.getElementById('signInRequiredProgressBar')
+        let signInRequiredText = document.getElementById('signInRequiredText')
 
-      signInRequiredProgressBar.style.display = "block"
-      signInRequiredText.style.display = "block"
+        signInRequiredProgressBar.style.display = "block"
+        signInRequiredText.style.display = "block"
 
-      // REENABLE THIS ONCE DEVELOPMENT IS DONE
-
-      // setTimeout(function() {
-      //     location.href = "./index.html"
-      // }, 2000)
-  }
+        setTimeout(function () {
+            location.href = "./index.html"
+        }, 2000)
+    }
 });
 
 
@@ -51,43 +48,62 @@ const db = getFirestore();
 
 const recordForm = document.getElementById("recordForm");
 
-recordForm.addEventListener("submit", (event) => {
+recordForm.addEventListener("submit", async (event) => {
     event.preventDefault();
 
+    const namaPenerima = document.getElementById("senaraiPengguna").value;
     const tarikh = document.getElementById("tarikh").value;
-    const jumlah = document.getElementById("jumlah").value;
-    const lokasi = document.getElementById("lokasi").value;
+    const noResit = document.getElementById("noResit").value;
+    const imbuhan = document.getElementById("imbuhan").value;
+    const panelKlinik = document.getElementById("panelKlinik").value;
 
     try {
         const recordData = {
             tarikh: tarikh,
-            jumlah: parseFloat(jumlah),
-            lokasi: lokasi
+            namaPenerima: namaPenerima,
+            noResit: noResit,
+            imbuhan: parseFloat(imbuhan),
+            panelKlinik: panelKlinik
         };
 
-        // Add new record document to the 'rekodElaun' collection
-        db.collection("rekodElaun").add(recordData)
-            .then((docRef) => {
-                console.log("New record document ID:", docRef.id);
-                // Clear the form inputs
-                recordForm.reset();
-            })
-            .catch((error) => {
-                console.error("Error adding record document:", error);
-            });
+        // Retrieve the pengguna document based on the selected namaPenerima
+        const penggunaQuerySnapshot = await getDocs(
+            query(collection(db, "pengguna"), where("nama", "==", namaPenerima))
+        );
+
+        if (!penggunaQuerySnapshot.empty) {
+            const penggunaDoc = penggunaQuerySnapshot.docs[0];
+
+            // Get the existing rekodImbuhan field
+            const existingRekodImbuhan = penggunaDoc.data().rekodImbuhan || [];
+
+            // Create an array to hold the existing rekodImbuhan and the new recordData
+            const updatedRekodImbuhan = [...existingRekodImbuhan, recordData];
+
+            // Update the rekodImbuhan field with the updated array
+
+            // Use setDoc to update the required field instead of updateDoc
+
+            await setDoc(penggunaDoc.ref, { rekodImbuhan: updatedRekodImbuhan }, { merge: true });
+
+            console.log("Record data appended successfully.");
+        } else {
+            console.log("Pengguna not found.");
+        }
+
     } catch (error) {
-        console.error("Error adding record document:", error);
+        console.error("Error appending record data:", error);
     }
 });
 
 // Function to fetch the list of teachers from the 'pengguna' collection
-async function fetchTeachers() {
+async function fetchPengguna() {
     try {
         // Fetch all documents from the 'pengguna' collection
         const querySnapshot = await getDocs(collection(db, "pengguna"));
 
         // Get the 'teacher' select element
-        const teacherSelect = document.getElementById("teacher");
+        const teacherSelect = document.getElementById("senaraiPengguna");
 
         // Clear existing options
         teacherSelect.innerHTML = "";
@@ -97,17 +113,17 @@ async function fetchTeachers() {
         defaultOption.value = "";
         defaultOption.disabled = true;
         defaultOption.selected = true;
-        defaultOption.textContent = "Select teacher";
+        defaultOption.textContent = "Pilih Nama";
         teacherSelect.appendChild(defaultOption);
 
         // Loop through the documents and create options for each teacher
         querySnapshot.forEach((doc) => {
-            const teacher = doc.data().nama;
+            const namaPengguna = doc.data().nama;
 
             // Create option element
             const option = document.createElement("option");
-            option.value = teacher;
-            option.textContent = teacher;
+            option.value = namaPengguna;
+            option.textContent = namaPengguna;
 
             // Add option to the select element
             teacherSelect.appendChild(option);
@@ -118,4 +134,4 @@ async function fetchTeachers() {
 }
 
 // Fetch teachers when the page is loaded
-window.addEventListener("DOMContentLoaded", fetchTeachers);
+window.addEventListener("DOMContentLoaded", fetchPengguna);

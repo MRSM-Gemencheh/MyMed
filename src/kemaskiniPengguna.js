bb69f175ac264e0f13ffa6ac4cf73004ae34796c
MagusWyvern <72914828+MagusWyvern@users.noreply.github.com>
2023-07-10 16:31:29 +0800

    Delete a user from the database

--- src/kemaskiniPengguna.js
+++ src/kemaskiniPengguna.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signOut } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, query, orderBy, getDocs, addDoc, where, doc, getDoc, setDoc } from "firebase/firestore";
+import { getFirestore, collection, query, orderBy, getDocs, addDoc, where, doc, getDoc, setDoc, deleteDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAzlMJB0WkUFXUXaD-GszRKxJayUo8tZQo",
-    authDomain: "e-medibalance.firebaseapp.com",
-    projectId: "e-medibalance",
-    storageBucket: "e-medibalance.appspot.com",
-    messagingSenderId: "117392039951",
-    appId: "1:117392039951:web:eee754ac35979a36af437a",
-    measurementId: "G-PG5LK8VTP9"
+  apiKey: "AIzaSyAzlMJB0WkUFXUXaD-GszRKxJayUo8tZQo",
+  authDomain: "e-medibalance.firebaseapp.com",
+  projectId: "e-medibalance",
+  storageBucket: "e-medibalance.appspot.com",
+  messagingSenderId: "117392039951",
+  appId: "1:117392039951:web:eee754ac35979a36af437a",
+  measurementId: "G-PG5LK8VTP9"
 };
 
 // Initialize Firebase
@@ -26,60 +26,60 @@ document.body.onload = function () {
 
   signOutButton.addEventListener('click', () => {
 
-      signOut(auth).then(() => {
-          location.reload()
-          // Sign-out successful.
-      }).catch((error) => {
-          // An error happened.
-      });
+    signOut(auth).then(() => {
+      location.reload()
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+    });
 
   })
 }
 
 auth.onAuthStateChanged(function (user) {
   if (user) {
-      // User is signed in
-      console.log(user);
+    // User is signed in
+    console.log(user);
 
-      userName.textContent = user.displayName;
-      signOutButton.style.display = "block";
+    userName.textContent = user.displayName;
+    signOutButton.style.display = "block";
 
-      // This is a crude implementation to disallow users from accessing the admin panel
-      // Users may be able to disable this part of the script and gets full admin access to create new documents
+    // This is a crude implementation to disallow users from accessing the admin panel
+    // Users may be able to disable this part of the script and gets full admin access to create new documents
 
-      // Check if user's email exists in 'admin' collection
-      const adminCollectionRef = collection(db, 'admin');
-      const queryRef = query(adminCollectionRef, where('emel', '==', user.email));
+    // Check if user's email exists in 'admin' collection
+    const adminCollectionRef = collection(db, 'admin');
+    const queryRef = query(adminCollectionRef, where('emel', '==', user.email));
 
-      getDocs(queryRef)
-          .then((querySnapshot) => {
-              if (querySnapshot.size > 0) {
-                  console.log("User is an admin."); // User's email exists in 'admin' collection
-                  // Perform actions for admin user
-              } else {
-                  console.log("User is not an admin."); // User's email doesn't exist in 'admin' collection
-                  setTimeout(function () {
-                      location.href = "./index.html"
-                  }, 2000)
-              }
-          })
-          .catch((error) => {
-              console.log("Error getting admin collection:", error);
-          });
+    getDocs(queryRef)
+      .then((querySnapshot) => {
+        if (querySnapshot.size > 0) {
+          console.log("User is an admin."); // User's email exists in 'admin' collection
+          // Perform actions for admin user
+        } else {
+          console.log("User is not an admin."); // User's email doesn't exist in 'admin' collection
+          setTimeout(function () {
+            location.href = "./index.html"
+          }, 2000)
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting admin collection:", error);
+      });
   } else {
-      // User is signed out
-      console.log("User is not logged in");
+    // User is signed out
+    console.log("User is not logged in");
 
-      let signInRequiredProgressBar = document.getElementById('signInRequiredProgressBar');
-      let signInRequiredText = document.getElementById('signInRequiredText');
+    let signInRequiredProgressBar = document.getElementById('signInRequiredProgressBar');
+    let signInRequiredText = document.getElementById('signInRequiredText');
 
-      signInRequiredProgressBar.style.display = "block";
-      signInRequiredText.style.display = "block";
+    signInRequiredProgressBar.style.display = "block";
+    signInRequiredText.style.display = "block";
 
-      // Redirect user back to sign-in page
-      setTimeout(function () {
-          location.href = "./index.html"
-      }, 2000)
+    // Redirect user back to sign-in page
+    setTimeout(function () {
+      location.href = "./index.html"
+    }, 2000)
   }
 });
 
@@ -144,7 +144,7 @@ updateUserForm.addEventListener('submit', (event) => {
     emel: emelMicrosoft,
     statusPerkahwinan: statusPerkahwinan
     // Update other fields as needed
-  } , { merge: true })
+  }, { merge: true })
     .then(() => {
       console.log('Document successfully updated');
       // Redirect or perform other actions upon successful update
@@ -152,4 +152,33 @@ updateUserForm.addEventListener('submit', (event) => {
     .catch((error) => {
       console.log('Error updating document:', error);
     });
+
+  setTimeout(function () {
+    location.href = "./admin.html"
+  }, 2000)
 });
+
+const deleteButton = document.getElementById('padamButton');
+
+deleteButton.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  // Confirm action with alert
+  if (confirm('Adakah anda pasti untuk memadam pengguna ini?')) {
+    // Delete the document
+    deleteDoc(penggunaDocRef)
+      .then(() => {
+        console.log('Document successfully deleted');
+        // Redirect or perform other actions upon successful delete
+      })
+      .catch((error) => {
+        console.log('Error deleting document:', error);
+      });
+  }
+
+  setTimeout(function () {
+    location.href = "./admin.html"
+  }, 2000)
+
+}
+);
